b6f90378fd41fc52b7f4b5d7a636ebfe36daabe6
TAKAMI Torao <koiroha@gmail.com>
2022-08-06 20:07:20 +0900

    add config for grcov

--- src/test.rs
+++ src/test.rs
@@ -3,16 +3,27 @@ use crate::Error;
 
 #[test]
 fn error_attributes() {
-  let err = Error::<char>::Unmatched {
-    location: Location::default(),
-    prefix: String::default(),
-    expecteds: Vec::default(),
-    expected_syntaxes: Vec::default(),
-    actual: String::default(),
-  };
-  let _ = format!("{:?}", err);
-  let _ = format!("{}", err);
-  assert_eq!(err, err);
-  assert!(err.eq(&err));
-  assert!(!err.ne(&err));
+  for err in [
+    Error::<char>::Unmatched {
+      location: Location::default(),
+      prefix: String::default(),
+      expecteds: Vec::default(),
+      expected_syntaxes: Vec::default(),
+      actual: String::default(),
+    },
+    Error::MultipleMatches {
+      location: Location::default(),
+      prefix: String::default(),
+      expecteds: Vec::default(),
+      actual: String::default(),
+    },
+    Error::UndefinedID(String::default()),
+    Error::Previous,
+  ] {
+    let _ = format!("{:?}", err);
+    let _ = format!("{}", err);
+    assert_eq!(err, err);
+    assert!(err.eq(&err));
+    assert!(!err.ne(&err));
+  }
 }
